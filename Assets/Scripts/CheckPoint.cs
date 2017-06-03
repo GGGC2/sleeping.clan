e71d9d194d3d9ebb4e5665f5e5477760daca31bf
dlwogml46 <dlwogml46@gmail.com>
2017-06-03 16:49:03 +0900

    Scripts
    CheckPoints, Respawn, Jumping, Moving,

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+	public MapManager mapManager;
+	// Use this for initialization
+	void Start () {
+		mapManager = FindObjectOfType<MapManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+			
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.name == "Boy"){
+			Debug.Log("Checkpoint Activated" + transform.position);
+			mapManager.currentCheckpoint = gameObject;
+		}
+	
+	}
+}
+ 
---
+++ Assets/Scripts/SlayPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlayPlayer : MonoBehaviour {
+public MapManager mapManager;
+
+	// Use this for initialization
+	void Start () {
+		mapManager = FindObjectOfType<MapManager>();
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.name == "Boy"){
+
+		 mapManager.RespawnPlayer();
+		}
+	}
+}
